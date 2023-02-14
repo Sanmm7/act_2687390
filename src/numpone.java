4e6655aa28c9cce3c90a82866c10d78846c693ce
alex avila <jaavila74@misena.edu.co>
2023-02-14 11:05:07 -0500

    Añadido un nuevo archivo llamado numpone por sann7

---
+++ src/numpone.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class numpone {
+    public static void main(String[] args) throws Exception {
+        Scanner lectura = new Scanner(System.in);
+        int number;
+        System.out.println("Ingrese un numero");
+        number= lectura.nextInt();
+        if(number==0){
+            System.out.println("Numero neutro");
+
+
+
+        }
+        else if(number>=1){
+
+            System.out.println("Numero positivo");
+
+        }
+        else{
+            System.out.println("Numero negativo");
+
+
+        }
+
+        lectura.close();
+
+    }
+}

fcfb584d26e4237f13d4e8ffdf5eb6eddc11e991
Jonsu <110569770+Sanmm7@users.noreply.github.com>
2023-03-14 11:51:48 -0500

    Se crea archivo persona en java reto de clases

---
+++ src/Persona.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+import java.util.Scanner;
+
+
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private Date fechaNacimiento;
+    private double sueldo;
+    public String getNombre(){
+        return nombre;
+        
+        
+        }
+        public int getEdad(){
+        return edad;
+
+
+
+        }
+        public Date getFechaNacimiento(){
+       return fechaNacimiento;
+
+
+        }
+        public double getSueldo(){
+            return sueldo;
+     
+     
+             }
+}
+
